669163812f0118bc190f2bd06b22f90d8f01f0ad
Jordany Rosas <jordanyryan@gmail.com>
2018-02-28 19:57:49 -0500

    Added folders, and imported packages to files who needed them, and setup react-redux, redux-form, and react-router

---
+++ README.md
@@ -0,0 +1,13 @@
+# React-Redux Boiler
+####A boiler plate with pre-installed react, redux, redux-form, react-router, axios, and lodash
+
+To clone this repo, paste this code into your terminal 
+> git clone https://github.com/jordanyryan/react-router-boiler.git  
+
+Then run 
+> npm install 
+ 
+Then  
+> npm start
+
+Enjoy
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  ,
+  document.getElementById('root')
+);
 registerServiceWorker();
