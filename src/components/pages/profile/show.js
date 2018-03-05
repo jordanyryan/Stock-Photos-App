2b58584d5b8f13bba5f077f9ba5cf4779d02d6e0
Jordany Rosas <jordanyryan@gmail.com>
2018-03-04 19:53:35 -0500

    Started planning and designing page and features

--- src/components/pages/profile/show.js
+++ src/components/pages/profile/show.js
@@ -26,8 +26,12 @@ class ShowPage extends Component {
   render() {
     if (!this.state.firstName) return null;
     return (
-      <div className="jumbotron jumbotron-fluid" id="profile-jumbo">
+      <div className="jumbotron jumbotron-fluid text-center" id="profile-jumbo">
           <h1>{`${this.state.firstName} ${this.state.lastName}`}</h1>
+          <div>Followers</div>
+          <div>Following</div>
+          <div>Collections</div>
+          <div>Liked Photos</div>
       </div>
     )
   }
