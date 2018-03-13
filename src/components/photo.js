6007c871cde5b396ae10b9a2b5a4060a9d396222
Jordany Rosas <jordanyryan@gmail.com>
2018-03-13 17:45:23 -0400

    Began on overlay of images

--- src/components/photo.js
+++ src/components/photo.js
@@ -22,11 +22,17 @@ class Photo extends Component {
   }
 
   render() {
-    let overlayClass = this.state.hover ? "overlay" : "overlay d-none";
+    const overlayClass = this.state.hover ? "overlay" : "overlay d-none";
     const {photo, itemNum} = this.props
     return(
+
       <div onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} className="col-lg-4 col-md-6 col-sm-12 img-gal my-2">
-        <img className="img img-fluid" src={photo.src} alt={`galItem${itemNum}`}/>
+        <div className="container-fluid p-0">
+          <img className="img img-fluid" src={photo.src} alt={`galItem${itemNum}`}/>
+          <div className={overlayClass}>
+          </div>
+        </div>
+        
       </div>
     )
   }
