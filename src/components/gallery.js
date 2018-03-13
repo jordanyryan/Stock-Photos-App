3ceb0f53af37c889f7cb3cbb2a308d7be32eafd7
Jordany Rosas <jordanyryan@gmail.com>
2018-03-13 15:54:52 -0400

    Fixed styling for gallery of images

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,8 +5,8 @@ class PhotoGallery extends Component {
   renderPhotos() {
     return this.props.photos.map((photo, i) => {
       return (
-        <div key={i} className="col-lg-4 col-md-6 col-sm-12 img-gal my-3">
-          <img className="img img-fluid img-border" src={photo.src} alt={`galItem${i}`}/>
+        <div key={i} className="col-lg-4 col-md-6 col-sm-12 img-gal my-2">
+          <img className="img img-fluid" src={photo.src} alt={`galItem${i}`}/>
         </div>
       )
     })
@@ -14,9 +14,13 @@ class PhotoGallery extends Component {
   
   render() {
     return(
-        <div className="row img-row mx-0 py-3">
-          {this.renderPhotos()}
+      <div className="container gal-container">
+        <div className="row-container">
+          <div className="row img-row mx-0 py-3 justify-content-between">
+            {this.renderPhotos()}
+          </div>
         </div>
+      </div>
     )
   }
 }
