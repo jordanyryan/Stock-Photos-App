c444c576f15251553a643b9c4ec15a1b47c0c1df
Jordany Rosas <jordanyryan@gmail.com>
2018-03-13 23:51:47 -0400

    Added buttons to image overlay that will allow a user to like, comment, and add to a collection

--- src/components/photo.js
+++ src/components/photo.js
@@ -22,14 +22,20 @@ class Photo extends Component {
   }
 
   render() {
-    const overlayClass = this.state.hover ? "overlay" : "overlay d-none";
+    const overlayClass = this.state.hover ? "overlay d-flex flex-column justify-content-between" : "overlay d-flex flex-column align-items hidden";
     const {photo, itemNum} = this.props
     return(
 
-      <div onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} className="col-lg-4 col-md-6 col-sm-12 img-gal my-2">
-        <div className="container-fluid p-0">
+      <div  className="col-lg-4 col-md-6 col-sm-12 img-gal my-2">
+        <div onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} className="container-fluid img-gal-container p-0">
           <img className="img img-fluid" src={photo.src} alt={`galItem${itemNum}`}/>
           <div className={overlayClass}>
+            <h2>Image Content</h2>
+            <div className="container img-icon-list h-25 align-items">
+              <i className="fa fa-heart fa-3x text-white mx-4"></i>
+              <i className="fa fa-plus fa-3x text-white mx-4"></i>
+              <i className="fa fa-comments fa-3x text-white mx-4"></i>
+            </div>
           </div>
         </div>
         
