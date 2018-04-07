aacb6a4a98738b2a3eee0a8594dba0f3f03790ea
Jordany Rosas <jordanyryan@gmail.com>
2018-04-06 22:59:22 -0400

    added a counter for liked photos

--- src/components/pages/profile/show.js
+++ src/components/pages/profile/show.js
@@ -7,21 +7,33 @@ import avatar from '../../../img/avatar_placeholder.png';
 
 
 class ShowPage extends Component {
-  componentWillMount() {
-    if (!this.props.user._id) {
-      localStorage.getItem('token') ? this.props.fetchUser() : this.props.history.push('/');
-    }
+  componentWillMount() {  
+    this.authenticateUser();
   }
+
+  componentDidUpdate() {
+    this.authenticateUser();
+  }
+
+  authenticateUser() {
+    this.props.authenticated ? this.props.fetchUser() : this.props.history.push('/');
+  }
+
 
   render() {
     if (!this.props.user._id) return null;
-    const {firstName, lastName} = this.props.user;
+    const {firstName, lastName, likedPhotos} = this.props.user;
     return (
       <div className="container-fluid p-0 m-0">
         <div className="jumbotron jumbotron-fluid text-center bg-dark m-0 p-0" id="profile-jumbo">
         <Navi textColor={"dark"} navColor={"light"}/>
           <h1>{`${firstName} ${lastName}`}</h1>
           <img className="avatar mb-3" src={avatar} alt="blank_avatar"/>
+          <div className="container">
+            <div className="row">
+              <p>Photos Liked: {likedPhotos.length}</p>
+            </div>
+          </div>
         </div>
         <PhotoGallery photos={this.props.user.likedPhotos} header={"Liked Photos"}/>
       </div>
@@ -29,8 +41,8 @@ class ShowPage extends Component {
   }
 }
 
-function mapStateToProps({user}) {
-  return {user};
+function mapStateToProps({auth, user}) {
+  return {authenticated: auth.authenticated, user};
 }
 
 export default connect(mapStateToProps, actions)(ShowPage);
